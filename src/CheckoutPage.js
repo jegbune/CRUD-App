81b906c620dfa5bb8db684efdb0e571c51edc059
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-16 00:11:16 +0100

    React lesson with chatGPT1

---
+++ src/CheckoutPage.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+const CheckoutPage = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission or data handling logic here
+    console.log('Form submitted:', { firstName, lastName, email, address });
+    // Reset form fields
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setAddress('');
+  };
+
+  return (
+    <div className="checkout-page">
+      <h2>Checkout</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="address">Address:</label>
+          <textarea
+            id="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Place Order</button>
+      </form>
+    </div>
+  );
+};
+
+export default CheckoutPage;
