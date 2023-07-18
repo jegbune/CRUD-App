9e462fb7b84916ae5a3c4d9159851bd6297da5fd
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-19 00:34:50 +0100

    React App on E-commerce Frontend Page with Chat GPT

---
+++ src/E-commercePage.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './E-commercePage.css';
+
+const productsData = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 10.99,
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 19.99,
+    image: 'https://via.placeholder.com/150',
+  },
+  // Add more products as needed
+];
+
+const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== productId);
+    setCartItems(updatedCartItems);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>E-Commerce Website</h1>
+      </header>
+      <div className="product-list">
+        {productsData.map((product) => (
+          <div key={product.id} className="product-card">
+            <img src={product.image} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>${product.price.toFixed(2)}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+      <div className="cart">
+        <h2>Cart</h2>
+        {cartItems.length === 0 ? (
+          <p>Your cart is empty.</p>
+        ) : (
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+                {item.name} (${item.price.toFixed(2)})
+                <button onClick={() => removeFromCart(item.id)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
