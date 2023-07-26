47ab72509b442f0b6848d4d8ca3f6005ef6aab07
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-27 00:36:57 +0100

    Dynamic Hero Component

--- src/routes/About.js
+++ src/routes/About.js
@@ -7,9 +7,10 @@ function About() {
         <>
         <NavBar />
         <Hero 
-        cName= 'hero'
-        heroImg= 'https://plus.unsplash.com/premium_photo-1661963922055-ab62205fae9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+        cName= 'hero-mid'
+        heroImg= 'https://images.unsplash.com/photo-1614130020598-093435d7f1a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
         title = 'About'
+        btnClass = 'hide'
         />
         </>
     )
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import NavBar from '../components/Navbar'
+import Hero from '../components/Hero'
+
 
 function Contact() {
     return (
         <>
         <NavBar />
-        <h1>This is Contact</h1>
+        <Hero 
+        cName= 'hero-mid'
+        heroImg= 'https://images.unsplash.com/photo-1614130020598-093435d7f1a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+        title = 'Contact'
+        btnClass = 'hide'
+        />
         </>
     )
 }
