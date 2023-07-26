1f560dad86509d100075eadd1fbe34ec69eb2e17
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-26 20:45:31 +0100

    React project on navBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ class Navbar extends Component {
                     <i className={this.state.clicked ? "fas fa-times" :  "fas fa-bars"}></i>
                 </div>
 
-                <ul className={this.state.clicked ? 'nav-menu  active' : 'nav-menu' }>
+                <ul className={this.state.clicked ? 'nav-menu.active' : 'nav-menu' }>
                     {MenuItems.map((item,index)=>{
                         return(
                     <li key={index}>
