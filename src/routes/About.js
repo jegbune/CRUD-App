c8d0129db0a7bf4f3d8b9c3bec1ea083aa0742e0
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-27 00:45:25 +0100

    Dynamic Hero Component

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import NavBar from '../components/Navbar'
 import Hero from '../components/Hero'
-
+import AboutImg from '../assets/img2.jpg'
 function About() {
     return (
         <>
         <NavBar />
         <Hero 
         cName= 'hero-mid'
-        heroImg= 'https://images.unsplash.com/photo-1614130020598-093435d7f1a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+        heroImg= {AboutImg}
         title = 'About'
         btnClass = 'hide'
         />
