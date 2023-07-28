58df88020a166530d3a45279436aa8532e6f9b08
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-28 21:59:10 +0100

    Create Footer for the website

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import './FooterStyles.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className='top'>
+                <div>
+                    <h1>Trippy</h1>
+                    <p>Choose your favourite destination.</p>
+                </div>
+                <div></div>
+            </div>
+
+            <div className='bottom'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer
