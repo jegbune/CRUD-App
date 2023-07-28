293319f0a777ac51d91e72636e68a4df2c65002c
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-28 21:48:07 +0100

    Create Cards for trips finale

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -2,8 +2,8 @@ import React from 'react'
 import TripData from './TripData'
 import './TripStyles.css'
 import Trip1 from '../assets/img1.jpg'
-import Trip2 from '../assets/img1.jpg'
-import Trip3 from '../assets/img1.jpg'
+import Trip2 from '../assets/img2.jpg'
+import Trip3 from '../assets/img3.jpg'
 
 function Trip() {
     return (
@@ -19,7 +19,16 @@ function Trip() {
         image = {Trip1}
         heading = 'Trip in Indonesia'
         text = 'Indonesia officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the indian and Pacific oceans. it consists of over 17,000 islands, including Sumatra, Java, Sulwest, and parts of Bornea and New Guinea'
-
+         />
+        <TripData 
+        image = {Trip2}
+        heading = 'Trip in Indonesia'
+        text = 'Indonesia officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the indian and Pacific oceans. it consists of over 17,000 islands, including Sumatra, Java, Sulwest, and parts of Bornea and New Guinea'
+         />
+        <TripData 
+        image = {Trip3}
+        heading = 'Trip in Indonesia'
+        text = 'Indonesia officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the indian and Pacific oceans. it consists of over 17,000 islands, including Sumatra, Java, Sulwest, and parts of Bornea and New Guinea'
          />
 
         </div>
--- src/components/TripData.js
+++ src/components/TripData.js
@@ -3,7 +3,9 @@ import './TripStyles.css'
 function TripData(props) {
     return (
          <div class= 't-card'>
+            <div className='t-image'>
             <img alt='t-image' src={props.image} />
+            </div>
             <h4>
                 {props.heading}
             </h4>
