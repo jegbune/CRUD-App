658d5be54a84c09ab69ca9beaac2d2f2d8ebc1a7
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-30 22:55:51 +0100

    Create ContactForm component for the website

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,6 +3,7 @@ import NavBar from '../components/Navbar'
 import Hero from '../components/Hero'
 import ContactImg from '../assets/img3.jpg'
 import Footer from '../components/Footer'
+import ContactForm from '../components/ContactForm'
 
 function Contact() {
     return (
@@ -14,6 +15,7 @@ function Contact() {
         title = 'Contact'
         btnClass = 'hide'
         />
+        <ContactForm />
         <Footer />
         </>
     )
