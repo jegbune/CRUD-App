9122d171b3b6f9744491720ba52f72e02cb74279
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-07-31 23:27:32 +0100

    Uncomment the Routes and Route of the website

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ import Contact from './routes/Contact';
 function App() {
   return (
     <div className="App">
-      
-      {/* <Routes>
+
+      <Routes>
         <Route path='/' element={<Home/>} /> 
         <Route path='/about' element={<About/>} /> 
         <Route path='/service' element={<Service/>} /> 
         <Route path='/contact' element={<Contact/>} /> 
-      </Routes> */}
+      </Routes>
       {/* <EcommercePage /> */}
       {/* <CartPage /> */}
       {/* <CheckoutPage /> */}
